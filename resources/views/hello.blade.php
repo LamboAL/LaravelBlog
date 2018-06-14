516e0bce48cd8e16bfe42f12bd9f3637b324623b
Lamb <groleksiy@gmail.com>
2018-06-14 21:32:23 +0300

    Init blog

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Hello <?php echo $name.' '.$fname;?></h1>
+  </body>
+</html>
